447537dd50e3981f14af95ebdaaf764b81c764f9
Paul Pham <paul@aquaron.com>
2020-10-14 23:43:38 -0700

    Update test in hope to get it pass

--- test/test.js
+++ test/test.js
@@ -2,11 +2,11 @@ const time = require('../dist/index');
 
 describe('toDate()', () => {
   const tests = [
-    { date: '2020-10-14', time: undefined, result: 1602658800000 },
-    { date: '2020-10-12', time: '12:22:33', result: 1602530553000 }
+    { date: '2020-10-14', time: undefined, result: '2020-10-14T00:00:00' },
+    { date: '2020-10-12', time: '12:22:33', result: '2020-10-12T12:22:33' }
   ];
   tests.forEach( (t) => test(`Date ${t.date}`, () =>
-    expect(time.toDate(t.date, t.time).getTime()).toEqual(t.result)
+    expect(time.toDate(t.date, t.time)).toEqual(new Date(t.result))
   ));
 
   const wrong_time = '12:2233';
