71798577244c63ce10cf2138d89f799736d3f1ea
Paul Pham <paul@aquaron.com>
2021-01-22 11:29:56 -0800

    test: Add testing for prevDOW()

--- test/DOW.test.js
+++ test/DOW.test.js
@@ -1,10 +1,21 @@
 const time = require('../dist/index');
 
+const dowName = [
+  'Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'
+];
+
 describe('nextDow()', () => {
   for(let i=0; i<7; ++i) {
-    test(`DOW ${i}`, () => {
-      const t = time.nextDow(i);
-      expect(t.getDay()).toEqual(i);
-    });
+    const t = time.nextDow(i);
+    test(`(${i}) ${dowName[i]} -> ${t.toUTCString()}`,
+      () => expect(t.getDay()).toEqual(i));
+  }
+});
+
+describe('prevDow()', () => {
+  for(let i=0; i<7; ++i) {
+    const t = time.prevDow(i);
+    test(`(${i}) ${dowName[i]} -> ${t.toUTCString()}`,
+      () => expect(t.getDay()).toEqual(i));
   }
 });
